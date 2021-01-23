b74507647c8b857a4636312779ccc7a9e502746c
shalom dawit <shalom.dawit@gmail.com>
2021-01-22 21:46:58 -0500

    footer change

--- portafoglio/src/components/Footer/index.js
+++ portafoglio/src/components/Footer/index.js
@@ -3,12 +3,18 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer container">
-      <div className="row">
-      <div className="col-xs-4 sig">This is how I
-      <span>DAWIT</span> { new Date().getFullYear() }</div>
-      </div>
-    </footer>
+    // <footer className="footer container">
+    //   <div className="row">
+    //   <div className="col-xs-4 sig">This is how I
+    //   <span>DAWIT</span> { new Date().getFullYear() }</div>
+    //   </div>
+    // </footer>
+    <footer>
+    <div id="text">created by SD_WORKS</div>
+    <div id="image">
+       <img src={require("../../assets/img/favicon.ico").default} alt={"juju"}/>8/20
+    </div>
+</footer>
   );
 }
 
