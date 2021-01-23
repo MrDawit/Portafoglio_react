9f84739c34dfd2f18007aa4063489f5152266ab7
shalom dawit <shalom.dawit@gmail.com>
2021-01-22 20:26:07 -0500

    added styling

--- portafoglio/src/pages/index.jsx
+++ portafoglio/src/pages/index.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 // import Navbar from '../components/Header';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import "../assets/css/index.css";
 
 const MainPage = () => {
 
