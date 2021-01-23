aa530ac44421c510416ebe64b6087957b7466b58
shalom dawit <shalom.dawit@gmail.com>
2021-01-22 20:08:47 -0500

    assignment requirement fix (Header)

--- portafoglio/src/components/Header/index.js
+++ portafoglio/src/components/Header/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./style.css";
+import { Link } from 'react-router-dom';
 
-
-
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Header() {
   return (
-    <div className="Header">
-      <h1>This is ME!</h1>
-      <h1>Welcome to My Portfolio </h1>
-            <div className="Header-Subtitle">How You Like Me Now...</div>
-            
-    </div>
-  )
+    <nav class="navbar">
+                <h2>Shalom Dawit</h2>
+                <form>
+                <Link id="red" to="/main">ABOUT</Link>               
+                    <Link id="green" to="/portfolio">PORTFOLIO</Link>
+                    <Link id="blue" to="/contact">CONTACT</Link> 
+                </form>
+            </nav>
+  );
 }
 
 export default Header;
