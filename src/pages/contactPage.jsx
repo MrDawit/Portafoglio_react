4dd1f58f8d012f837932e2c500ccb2df5d0097d5
shalom dawit <shalom.dawit@gmail.com>
2021-02-01 16:44:39 -0500

    moved form from contactPage to new ContactForm component

--- src/pages/contactPage.jsx
+++ src/pages/contactPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactForm from '../components/ContactForm';
 import Spacer from '../components/Spacer';
 import "../assets/css/contactPage.css";
 
@@ -15,38 +16,13 @@ const ContactPage = () => {
 
             <main className="col-xs-12 col-lg-10">
 
-               
-                            <h1>Contact</h1>
-                            <form>
-                                <label for="name">Name:</label>
-                                <br/>
-                                <input type="text" id="name" name="name"/>
-                                <br/>
-                                <label for="email">Email:</label>
-                                <br/>
-                                <input type="text" id="email" name="email"/>
-                                <br/>
-                                <label for="message">Message:</label>
-                                <br/>
-                                <textarea id="message" name="message" cols="40"></textarea><br/>
-                                <input type="submit" value="SUBMIT"/>
-                                <br/><br/>
-                                <div id="contact_links"><img alt="LinkedIn_Image" src={require("../assets/img/icon-linkedin.png").default} />
-                                    <a href="https://www.linkedin.com/in/shalom-dawit-a0a5a4126"
-                                        target="_blank" rel="noreferrer">
-                                        Linkedin.com/in/Shalom-Dawit-a0a5a4126
-                                    </a>
-                                    <br/>
-                                    <img alt="Github_Image" src={require("../assets/img/github-logo.png").default} />
-                                    <a href={"https://github.com/MrDawit"} target="_blank" rel="noreferrer">
-                                        Github.com/MrDawit
-                                    </a>
-                                </div>
-                            </form>
-                        
-                   
+
+                <h1>Contact</h1>
+                <ContactForm />
+
+
             </main>
-<Spacer/>
+            <Spacer />
             {/* <Footer /> */}
         </div>
     )
