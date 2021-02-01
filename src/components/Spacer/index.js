23c761721c7e1ccf6df9b67b171b2532ef95f2bc
shalom dawit <shalom.dawit@gmail.com>
2021-02-01 15:40:51 -0500

    added globe image and styling to Spacer

--- src/components/Spacer/index.js
+++ src/components/Spacer/index.js
@@ -5,10 +5,13 @@ function Spacer() {
   return (
   
     <>
-  <div>
-    
+  <div >
+    <div id="globe">
+  <img id="globe_spacer" src={require('../../assets/img/globe3.png').default} alt={"spacer"}/>
+    </div>
     {/* <div id="image_spacer"> */}
        <img id="image_spacer" src={require("../../assets/img/spacer.jpg").default} alt={"spacer"}/>
+       
     </div>
 
 </>
