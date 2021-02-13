207e45e696f45b50b61e0dadc80efdc8fec0a98a
shalom dawit <shalom.dawit@gmail.com>
2021-02-13 01:44:39 -0500

    reverting back to prev commit prep

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,20 +5,15 @@ import {Container, Row, Col} from "react-bootstrap";
 
 function Footer() {
   return (
-<>
-    <Container  id="outer_footer">
-          <Row >
-            {/* <Col  lg={0} md={8} sm={4} className=""/> */}
-            <Col lg={12} md={4} sm={8} className="" id="inner_footer">
-              <div id="texting"><span>Created By </span>NEW DIGITAL WORLD</div>
-              <div id="image">
-                <img src={require("../../assets/img/favicon.ico").default} alt={"juju"} /> 
-                <span id="date">12/20</span>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </>
+  
+    <>
+    <footer id="outer_footer">
+    <div id="texting">Created By NEW DIGITAL WORLD</div>
+    <div id="image">
+       <img src={require("../../assets/img/favicon.ico").default} alt={"juju"}/> 12/20
+    </div>
+</footer>
+</>
   );
 }
 
--- client/src/pages/contactPage.jsx
+++ client/src/pages/contactPage.jsx
@@ -2,16 +2,98 @@ import React from 'react';
 import ContactForm from '../components/ContactForm';
 import Spacer from '../components/Spacer';
 import "../assets/css/contactPage.css";
+// import axios from 'axios';
 
 const ContactPage = () => {
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [subject, setSubject] = useState('');
+    // const [message, setMessage] = useState('');
+    // const submitRequest = async event => {
+    //     event.preventDefault();
+    //sending the request through axios also works nicely
+// axios.post('/api/contact',{ name,email, subject, message })
+    // };
+
+//WORKS
+// const [email, setEmail] = useState('');
+// const [message, setMessage] = useState('');
+// const submitRequest = async (e) => {
+//     e.preventDefault();
+//     console.log({ email, message });
+
+//     const url = '/api/contact';
+//     const options = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json;charset=UTF-8'
+//       },
+//       body: JSON.stringify({
+//          email,
+//          message
+//       })
+//     };
+    
+//     fetch(url, options)
+//       .then(response => {
+//         console.log(response.status);
+//       });
+// };
 
     return (
         <div className="bod">
+            {/* <Navbar /> */}
+            {/* <ul>
+                {["I ", "am a ", "Coder"].map((contact, idx) => {
+                    return <li key={idx}>{contact}</li>
+                })}
+            </ul> */}
+
             <main className="col-xs-12 col-lg-10">
+
+
                 <h1>Contact</h1>
                 <ContactForm />
+
+                        {/* WORKS
+                <form  onSubmit={submitRequest}>
+            <label >Name:</label>
+            <br />
+            <input type="text" id="name" name="name" onChange={e => setName(e.target.value)}
+              value={name}/>
+            <br />
+            <label >Email:</label>
+            <br />
+            <input type="text" id="email" name="email"  onChange={e => setEmail(e.target.value)}
+              value={email}
+              />
+            <br />
+            <label >Subject:</label>
+            <br />
+            <input type="text" id="subject" name="subject"  onChange={e => setSubject(e.target.value)}
+              value={subject}
+              />
+              <br />
+            <label >Message:</label>
+            <br />
+            <textarea id="message" type="text" name="message" cols="40" 
+            onChange={e => setMessage(e.target.value)}
+            value={message}
+            />
+            
+            <br />
+         
+            <input id="submitBtn" type="submit" value="Submit"/>
+            </form> */}
+
+
+
+
+
+
             </main>
             <Spacer />
+            {/* <Footer /> */}
         </div>
     )
 };
