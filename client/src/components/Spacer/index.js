111d5732f06f9f0c3d3263337a66ae3563eb9abb
shalom dawit <shalom.dawit@gmail.com>
2021-06-18 23:59:12 -0400

    adding scroll effect to #globe

--- client/src/components/Spacer/index.js
+++ client/src/components/Spacer/index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import "./style.css";
 
 function Spacer() {
+  const [small, setSmall] = useState(false);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", () =>
+        setSmall(window.pageYOffset > 600)
+      );
+    }
+  }, []);
+
+
   return (
   
     <>
   <div >
     <div id="globe">
-  <img id="globe_spacer" src={require('../../assets/img/globe3.png').default} alt={"spacer"}/>
+  <img id="globe_spacer" className={`img ${
+          small ? "small" : ""
+        }`} 
+        src={require('../../assets/img/globe3.png').default} alt={"spacer"}/>
     </div>
     {/* <div id="image_spacer"> */}
        <img id="image_spacer" src={require("../../assets/img/spacer.jpg").default} alt={"spacer"}/>
