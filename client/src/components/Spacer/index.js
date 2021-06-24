1d8e97daa59dae06b1bb3f5e18be72961094d05a
shalom dawit <shalom.dawit@gmail.com>
2021-06-24 05:20:57 -0400

    scrollY test

--- client/src/components/Spacer/index.js
+++ client/src/components/Spacer/index.js
@@ -2,24 +2,39 @@ import React, { useEffect, useState }  from "react";
 import "./style.css";
 
 function Spacer() {
-  const [small, setSmall] = useState(false);
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", () =>
-        setSmall(window.pageYOffset > 600)
-      );
-    }
-  }, []);
+  // const [small, setSmall] = useState(false);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     window.addEventListener("scroll", () =>
+  //       setSmall(window.pageYOffset > 600)
+  //     );
+  //   }
+  // }, []);
+
+  const [testColor, setTestColor] = useState("white")
+  const listenScrollEvent = () => {
+    window.scrollY > 500
+      ? setTestColor("yellow")
+      : setTestColor("pink")
+  }
+// Similar to componentDidMount and componentDidUpdate:
+useEffect(() => {
+  window.addEventListener("scroll", listenScrollEvent)
+})
 
 
   return (
   
     <>
+    
   <div >
+    <div style={{color: testColor}}>FOODDDDDDD</div>
     <div id="globe">
-  <img id="globe_spacer" className={`img ${
-          small ? "small" : ""
-        }`} 
+  <img id="globe_spacer" 
+  // className={`undefined ${
+  //         small ? "small" : ""
+  //       }`} 
+  
         src={require('../../assets/img/globe3.png').default} alt={"spacer"}/>
     </div>
     {/* <div id="image_spacer"> */}
