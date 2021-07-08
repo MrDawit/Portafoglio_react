f42da32363c39595f33f966b7b90fde84510bbb6
shalom dawit <shalom.dawit@gmail.com>
2021-07-08 00:58:05 -0400

    fixed mouse event and cleaned Spacer component a little

--- client/src/components/Spacer/index.js
+++ client/src/components/Spacer/index.js
@@ -1,31 +1,14 @@
-import React, { useEffect, useState, useCallback }  from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import "./style.css";
 
 function Spacer() {
 
-  const [state, setState] = useState({
-    newWidth: 200 / 2,
-    newBarAmount: 200,
-    isDragging: false
-  });
 
-
-  // const [small, setSmall] = useState(false);
-  // useEffect(() => {
-  //   if (typeof window !== "undefined") {
-  //     window.addEventListener("scroll", () =>
-  //       setSmall(window.pageYOffset > 600)
-  //     );
-  //   }
-  // }, []);
-  // const handleMove = (e) => {
-  //   e.nativeEvent.clientX
-  //   console.log("F U C K " + e.nativeEvent.clientX); // has value
-  // }
-  // const [testOpacity, setOpacity] = useState("")
+  const [backgroundColor, setBackgroundColor] = useState("")
   // const listenScrollEvent = () => {
+
   //    window.scrollY > 650 
-    
+
   //     // ? setOpacity(".2")
   //     // : setOpacity("1")
 
@@ -34,62 +17,53 @@ function Spacer() {
   // }
 
 
-  const handleMouseMove = useCallback(
+
+  //   const listenMoveEvent = () => {
+  const listenMouseMove = useCallback(
     ({ clientX }) => {
-      if (state.isDragging) {
-        setState(prevState => ({
-          ...prevState,
-          newWidth: clientX > 0 ? clientX / 2 : 0,
-          newBarAmount: clientX > 0 ? clientX : 0
-        }));
-      }
+      //  window.clientX > 100
+      clientX > 300
+        //  ? setOpacity(".2")
+        //  : setOpacity("1")
+        ? setBackgroundColor("purple")
+        // ? console.log("DID IT")
+        : setBackgroundColor("")
     },
-    [state.isDragging]
+    []
   );
-//   const listenMoveEvent = () => {
-//    window.clientX > 100
-//      ? setOpacity(".2")
-//      : setOpacity("1")
-//  }
 
-// Similar to componentDidMount and componentDidUpdate:
-useEffect(() => {
-  // window.addEventListener("scroll", listenScrollEvent);
-  // window.addEventListener("nativeEvent",listenMoveEvent);
-  window.addEventListener("mousemove", handleMouseMove);
+  useEffect(() => {
+    window.addEventListener("mousemove", listenMouseMove);
 
-// })
-return () => {
-  window.removeEventListener("mousemove", handleMouseMove);
-};
-}, [handleMouseMove]);
+    // return () => {
+    //   window.removeEventListener("mousemove", listenMouseMove);
+    // };
 
-
+  });
 
   return (
-  
+
     <>
-    
-  <div style={{background: handleMouseMove}}>
-    {/* <div style={{color: testColor}}>FOOD for Thought-test</div> */}
-    <div id="globe">
-  <img id="globe_spacer" 
-  // className={`undefined ${
-  //         small ? "small" : ""
-  //       }`} 
+      <div style={{ background: backgroundColor }}>
+        {/* <div style={{color: testColor}}>FOOD for Thought-test</div> */}
+        <div id="globe">
+          <img id="globe_spacer"
+            // className={`undefined ${
+            //         small ? "small" : ""
+            //       }`} 
 
 
-  // style={{opacity: testOpacity}}
+            // style={{opacity: testOpacity}}
 
-  // style={{background: testOpacity}}
-        src={require('../../assets/img/globe3.png').default} alt={"spacer"}/>
-    </div>
-    {/* <div id="image_spacer"> */}
-       <img id="image_spacer" src={require("../../assets/img/spacer.jpg").default} alt={"spacer"}/>
-       
-    </div>
+            // style={{background: testOpacity}}
+            src={require('../../assets/img/globe3.png').default} alt={"spacer"} />
+        </div>
+        {/* <div id="image_spacer"> */}
+        <img id="image_spacer" src={require("../../assets/img/spacer.jpg").default} alt={"spacer"} />
 
-</>
+      </div>
+
+    </>
   );
 }
 
