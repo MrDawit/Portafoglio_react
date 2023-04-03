fd988717c76db46c0ae01306436b273d8bd87dff
Shalom Dawit <Shalom.Dawit@gmail.com>
2023-04-02 20:48:31 -0400

    ProjectContext.js: removed .default from imageURL links

--- client/src/utils/ProjectContext.js
+++ client/src/utils/ProjectContext.js
@@ -11,7 +11,7 @@ export const ProjectProvider = props => {
             
             name: "The ToolShed",
             descriptionShort: "A REACT app to help users not lose their tools!",
-            imageURL: require("../assets/img/toolshed.png").default,
+            imageURL: require("../assets/img/toolshed.png"),
             gitHubRepo:"https://github.com/MrDawit/ToolShed",
             deployURL:"https://toolshed-app-team8.herokuapp.com/",
             skills: "React, REST API, OKTA, BLK systems framework, Bootstrap, MySQL, NodeJS, Javascript, HTML, CSS",
@@ -21,7 +21,7 @@ export const ProjectProvider = props => {
            
             name: "Covid19 Info",
             descriptionShort: "Covid-19 high level statistics dashboard",
-            imageURL: require("../assets/img/covid19_info.png").default,
+            imageURL: require("../assets/img/covid19_info.png"),
             gitHubRepo:"https://github.com/troylochner/covid19-info",
             deployURL:"https://troylochner.github.io/covid19-info/",
             skills: "Web API, Materialize framework, Javascript, JQuery, HTML, CSS",
@@ -31,7 +31,7 @@ export const ProjectProvider = props => {
            
             name: "Recipe Tracker",
             descriptionShort: "An App to organize your recipes",
-            imageURL: require("../assets/img/main.png").default,
+            imageURL: require("../assets/img/main.png"),
             gitHubRepo:"https://github.com/lcalderin12/Recipe-Tracker",
             deployURL:"https://recipes-tracker-hanzee.herokuapp.com/",
             loginInfo:"* (For Login) EMAIL: Shalom.Dawit@gmail.com PASSWORD: food",
@@ -42,7 +42,7 @@ export const ProjectProvider = props => {
            
             name: "All U Can Burger",
             descriptionShort: "An App to organize your burger diet using express handlebars",
-            imageURL: require("../assets/img/AllUCanBurger.png").default,
+            imageURL: require("../assets/img/AllUCanBurger.png"),
             gitHubRepo:"https://github.com/MrDawit/All-U-Can-Burger",
             deployURL:"https://all-u-can-burger.herokuapp.com/",
             skills: "JavaScript, HTML5, CSS3, REST API, NodeJS, Express Handlebars, JQuery, AJAX, MySQL, Sequelize, Bootstrap, MVC design pattern ",
@@ -52,7 +52,7 @@ export const ProjectProvider = props => {
             
             name: "WeatherApp",
             descriptionShort: "An App giving weather forecast details to a location of choice",
-            imageURL: require("../assets/img/WeatherApp.png").default,
+            imageURL: require("../assets/img/WeatherApp.png"),
             gitHubRepo:"https://github.com/MrDawit/WeatherApp",
             deployURL:"https://MrDawit.github.io/WeatherApp",
             skills: "HTML, CSS, Web API, Javascript, Localstorage API, Bootstrap, DOM, Moment library",
@@ -62,7 +62,7 @@ export const ProjectProvider = props => {
            
             name: "Scheduler",
             descriptionShort: "An App organizing your tasks using localStorage",
-            imageURL: require("../assets/img/Scheduler.png").default,
+            imageURL: require("../assets/img/Scheduler.png"),
             gitHubRepo:"https://github.com/MrDawit/Scheduler",
             deployURL:"https://MrDawit.github.io/Scheduler",
             skills: "HTML, CSS, JQuery, Javascript, Localstorage API, Bootstrap, DOM, Moment library",
@@ -72,7 +72,7 @@ export const ProjectProvider = props => {
             
             name: "Google-Books",
             descriptionShort: "A REACT app saving your library of books to buy or find info on",
-            imageURL: require("../assets/img/google_books.png").default,
+            imageURL: require("../assets/img/google_books.png"),
             gitHubRepo:"https://github.com/MrDawit/Google-Books",
             deployURL:"https://google-books-hmwk.herokuapp.com/",
             skills: "React, CSS, REST API, MERN-stack, MongoDB, Mongoose, Node, Axios, Bootstrap",
@@ -82,7 +82,7 @@ export const ProjectProvider = props => {
             
             name: "Code Quiz: Basketball (Laker's Edition!)",
             descriptionShort: "Quiz/Tribute App for Kobe 'Bean' Bryant",
-            imageURL: require("../assets/img/Code_Quiz.png").default,
+            imageURL: require("../assets/img/Code_Quiz.png"),
             gitHubRepo:"https://github.com/MrDawit/Code-Quiz",
             deployURL:"https://mrdawit.github.io/Code-Quiz/index.html",
             skills: "HTML, CSS, JQuery, Javascript, Localstorage API, Bootstrap, DOM, Moment library",
