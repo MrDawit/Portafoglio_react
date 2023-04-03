20ed1b17efccaaf52b4609be5146d1e564393ba8
Shalom Dawit <Shalom.Dawit@gmail.com>
2023-04-02 20:51:59 -0400

    IconContext.js: removed .default from localAddress links

--- client/src/utils/IconContext.js
+++ client/src/utils/IconContext.js
@@ -9,73 +9,73 @@ export const SkillIconProvider = props => {
 
         {
             name: "html5",
-            localAddress: require("../assets/img/html5.png").default,
+            localAddress: require("../assets/img/html5.png"),
             id: 1
         },
 
         {
             name: "css3",
-            localAddress: require("../assets/img/css3.png").default,
+            localAddress: require("../assets/img/css3.png"),
             id: 2
         },
 
         {
             name: "javascript",
-            localAddress: require("../assets/img/JS.png").default,
+            localAddress: require("../assets/img/JS.png"),
             id: 3
         },
 
         {
             name: "jquery",
-            localAddress: require("../assets/img/jQuery.png").default,
+            localAddress: require("../assets/img/jQuery.png"),
             id: 4
         },
 
         {
             name: "mysql",
-            localAddress: require("../assets/img/MySQL.jpg").default,
+            localAddress: require("../assets/img/MySQL.jpg"),
             id: 5
         },
 
         {
             name: "mongodb",
-            localAddress: require("../assets/img/mongodb.png").default,
+            localAddress: require("../assets/img/mongodb.png"),
             id: 6
         },
 
         {
             name: "react",
-            localAddress: require("../assets/img/React.png").default,
+            localAddress: require("../assets/img/React.png"),
             id: 7
         },
 
         {
             name: "mern",
-            localAddress: require("../assets/img/mern.jpg").default,
+            localAddress: require("../assets/img/mern.jpg"),
             id: 8
         },
 
         {
             name: "nodeJS",
-            localAddress: require("../assets/img/nodeJS.png").default,
+            localAddress: require("../assets/img/nodeJS.png"),
             id: 9
         },
 
         {
             name: "npm",
-            localAddress: require("../assets/img/npm_logo.png").default,
+            localAddress: require("../assets/img/npm_logo.png"),
             id: 10
         },
 
         {
             name: "express",
-            localAddress: require("../assets/img/express.png").default,
+            localAddress: require("../assets/img/express.png"),
             id: 11
         },
 
         {
             name: "bootsrap",
-            localAddress: require("../assets/img/bootstrap.png").default,
+            localAddress: require("../assets/img/bootstrap.png"),
             id: 12
         },
     ]);
