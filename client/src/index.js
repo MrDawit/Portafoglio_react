04abc0c273d922a1b5c5d58cee766e09fce7a2be
Shalom Dawit <Shalom.Dawit@gmail.com>
2024-07-14 10:06:03 -0400

    returned reportWebVitals to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import {createRoot} from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
@@ -26,4 +26,4 @@ const root = createRoot(container);
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-//reportWebVitals();
+reportWebVitals();
